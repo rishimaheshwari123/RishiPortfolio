6726b279ef52c2fd6c0e1d240b5d0edc4b6450df
Rishi <114659322+rishimaheshwari123@users.noreply.github.com>
2023-02-20 10:19:14 +0530

    update

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hello there 👋, I am Rishi Maheshwari</span> and I am form Kothri (MP) I have completed my graduation form BU BHOPAL university  and I am currently pursuing MCA form VIT BHOPAL university. My Intrest is in web devlopmet and  I am currently pursuing for MERN Stack devloper,and I have good problem solving skill and my hobby is play cricket."
+        "<span>Hello there 👋, I am Rishi Maheshwari</span> and I am form Kothri (MP) I have completed my graduation form BU BHOPAL university  and I am currently pursuing MCA form VIT BHOPAL university. My Interest is in web development and  I am currently pursuing for MERN Stack developer,and I have good problem solving skill and my hobby is play cricket."
       ],
       typeSpeed: 7,
       contentType: 'html'
